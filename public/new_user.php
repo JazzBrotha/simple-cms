9e4b0aa5338ba2a93255015b426a5a38cc2db1c0
jesperengstrom <jengstro@gmail.com>
2017-05-03 13:05:24 +0200

    moved some files around

--- public/new_user.php
+++ public/new_user.php
@@ -1,7 +1,6 @@
 <?php
 require "../classes/user.php";
-require "../resources/error.php";
-require "../resources/pdo.php";
+require "../start.php";
 
 $new_user = new User($_POST["username"], $_POST["password"], $_POST["firstname"], $_POST["lastname"], $_POST["email"], $_POST["profession"], $_POST["description"], $pdo);
     $statement = $new_user->store_user();
