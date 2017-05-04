69850657d8cb3401cd136eb2e651eb2ac5821bbb
JazzBrotha <mattiasostblom@protonmail.com>
2017-05-04 15:59:24 +0200

    implemented icons and new blog template

--- app/views/page/show.php
+++ app/views/page/show.php
@@ -1,18 +1,37 @@
-<?php require VIEW_ROOT . '/public/templates/header.php';
-?>
+<?php require VIEW_ROOT . '/public/templates/header.php';?>
+<header class="masthead" style="background-image:url(<?php echo BASE_URL; ?>/assets/img/post-bg.jpg)">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                <div class="post-heading">
+                  <?php if (!$page) { ?>
+                    <h1>No post found, sorry.</h1>
 
-  <?php if (!$page) { ?>
-    <p>No post found, sorry.</p>
+                  <?php } else { ?>
+                    <h1><?php echo escape($page['title']); ?></h1>
 
-  <?php } else { ?>
-    <h2><?php echo escape($page['title']); ?></h2>
-    <?php echo escape($page['body']); ?>
+                    <h2 class="subheading">Summary</h2>
+                  <span class="meta">Posted by <a href=""><?php echo $page['user_id']; ?></a> on <?php echo $page['created']->format('jS M Y'); ?></span>
+                  <?php if ($page['updated'] !== null) { ?>
+                  <span class="meta">Last updated on <?php echo $page['updated']->format('jS M Y h:i a'); ?></span>
+                  <?php } ?>
+                  <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
 
-    <p>Created on <?php echo $page['created']->format('jS M Y'); ?>
-      <?php if ($page['updated'] !== null) { ?>
-      Last updated on <?php echo $page['updated']->format('jS M Y h:i a'); ?></p>
-    <?php } ?>
-    <?php } ?>
 
+<!-- Post Content -->
+<article>
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+      <?php echo escape($page['body']); ?></div>
+          </div>
+      </div>
+  </article>
 
+   <hr>
 <?php require VIEW_ROOT . '/public/templates/footer.php'; ?>
