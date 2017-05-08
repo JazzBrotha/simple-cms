92c9e93155f2d7db24d67775cad0cde1283d7712
jesperengstrom <jengstro@gmail.com>
2017-05-08 13:40:19 +0200

    likes count working

--- app/classes/likes.php
+++ app/classes/likes.php
@@ -1,11 +1,12 @@
 <?php
 
 class Likes {
-    public static function count_likes($postId) {
-        return 5;
-        // $pdo->prepare("
-        // SELECT ")
-
+    public static function count_likes($postId, $pdo) {
+        $likes = $pdo->prepare(
+        "SELECT COUNT(*) FROM likes
+        WHERE post_id = $postId;");
+        $likes->execute();
+        $result = $likes->fetch(PDO::FETCH_ASSOC);
+        return $result['COUNT(*)'];
     }
-
 }
