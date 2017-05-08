dc5cae97058da648be9bea770d9d99391ff9643e
jesperengstrom <jengstro@gmail.com>
2017-05-08 11:09:36 +0200

    admin panel->edit profile now working

--- user/add.php
+++ user/add.php
@@ -3,10 +3,11 @@
 require '../app/start.php';
 require APP_ROOT . '/classes/post.php';
 
-if (!empty($_POST)) {
-
   //Måste ändras till variabeln i Session
   $userId = 1;
+
+if (!empty($_POST)) {
+
   $insertPage = new Post($userId, $_POST['title'], $_POST['body'], $_POST['tags'], $pdo);
   $insertPage->store_post();
 
