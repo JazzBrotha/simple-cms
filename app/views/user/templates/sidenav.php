49867c0e99f7351563adf86bab39244a39d264fe
jesperengstrom <jengstro@gmail.com>
2017-05-10 11:11:07 +0200

    admin side panel

--- app/views/user/templates/sidenav.php
+++ app/views/user/templates/sidenav.php
@@ -2,14 +2,21 @@
   <div class="row">
     <nav class="col-sm-3 col-md-2 hidden-xs-down bg-inverse sidebar">
       <ul class="nav flex-column">
+                <?php
+        if ($_SESSION['is_admin']): ?>
         <li class="nav-item">
-          <a class="nav-link text-white" href="<?php echo BASE_URL; ?>/user/list.php">VIEW POSTS <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="#">VIEW ALL POSTS</a>
+        </li>
+        <div class="dropdown-divider"></div>
+        <?php endif; ?>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="<?php echo BASE_URL; ?>/user/list.php">VIEW YOUR POSTS<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="<?php echo BASE_URL; ?>/user/add.php">CREATE POST</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo BASE_URL; ?>/user/edit_user.php">EDIT PROFILE</a>
+          <a class="nav-link" href="<?php echo BASE_URL; ?>/user/edit_user.php">EDIT YOUR PROFILE</a>
         </li>
       <li class="nav-item">
           <a class="nav-link" href="<?php echo BASE_URL; ?>/public/logout.php">LOG OUT</a>
