684e8ea932a72e4693370556779472681f897204
Jesper Engström <jesperengstrom@users.noreply.github.com>
2017-05-10 09:30:35 +0200

    access control for edit & delete. delete sweetalert. (#20)

--- user/delete.php
+++ user/delete.php
@@ -1,18 +1,29 @@
 <?php
 
 require '../app/start.php';
+require APP_ROOT . '/classes/users.php';
+require APP_ROOT . '/classes/posts.php';
 
-//här måste vi kolla att användare är inloggad och är user_id för inlägget med post_id, annars kan man ta bort
-//andras inlägg genom att skriva in query
-
-if (isset($_GET['post_id'])) {
-  $deletePage = $pdo->prepare("
-    DELETE FROM posts
-    WHERE post_id = :id
-  ");
-  $deletePage->execute([':id' => $_GET['post_id']]);
+if (!isset($_SESSION)) {
+  session_start();
 }
 
-header('Location: ' . BASE_URL . '/user/list.php');
+$userId = $_SESSION['user_id'];
+$postId = $_GET['post_id'];
+//checking user right to delete page
+$hasAccess = Users::has_access($userId, $postId, $pdo);
+
+if ($hasAccess) {
+  Posts::delete_post($postId, $pdo);
+  header('Location: ' . BASE_URL . '/user/list.php?success=deleted');
+} else {
+    header('Location: ' . BASE_URL . '/user/list.php?access=false');
+  die();
+}
+
+if (!isset($_GET['post_id'])) {
+  header('Location: ' . BASE_URL . '/user/list.php');
+  die();
+}
 
 require VIEW_ROOT . '/user/delete.php';
