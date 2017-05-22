d1122f6b8769dc79c9e7eff97eac6bd018767b25
JazzBrotha <mattiasostblom@protonmail.com>
2017-05-22 15:35:11 +0200

    Admin can delete users

---
+++ user/admin_posts_list.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../app/start.php';
+require '../app/classes/posts.php';
+require APP_ROOT . '/classes/users.php';
+$userId = $_SESSION["user_id"];
+$user = $USERS->get_full_user($userId);
+
+if ($_SESSION['loggedin'] && $_SESSION['is_admin']) {
+    $currentPage = 'admin_posts_list.php';
+    $allPosts = $POSTS->get_all_posts();
+    require VIEW_ROOT . '/user/admin_posts_list.php';
+
+} else {
+    header('Location: ' . BASE_URL . '/user/list.php?access=denied');
+    die();
+}
