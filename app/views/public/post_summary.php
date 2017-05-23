73377b431122ece7790c8dc631a00c888c4b9ee5
evelinasundin <evelinasundin@hotmail.se>
2017-05-23 13:40:03 +0200

    Toggle (#55)
    * aha
    
    * session start destroy
    
    * css changes
    
    * nd
    
    * sidebar fixes, admin fix, profile pic added in sidebar, responsive tables
    
    * profile pic fixes in navbar
    
    * profile pic fixes and contact page
    
    * toggle in admin post list fixed
    
    * indentation
    
    * view all users toggle menu position fixed

--- app/views/public/post_summary.php
+++ app/views/public/post_summary.php
@@ -1,40 +1,39 @@
 <?php
 $plainTextBody = strip_tags($post['body']);
 $summary = substr($plainTextBody, 0, 50);?>
-
-<div class="post-preview">
-    <!--print title-->
-    <a href="<?php echo BASE_URL; ?>/page.php?post_id=<?php echo $post['post_id']; ?>">
-        <h2 class="post-title">
+    <div class="post-preview">
+        <!--print title-->
+        <a href="<?php echo BASE_URL; ?>/page.php?post_id=<?php echo $post['post_id']; ?>">
+            <h2 class="post-title">
             <?php echo escape($post['title']); ?>
         </h2>
-        <!--print summary-->
-        <h3 class="post-subtitle">
+            <!--print summary-->
+            <h3 class="post-subtitle">
             <?php echo $summary ?>...</h3>
-    </a>
-    <!--print author-->
-    <p class="post-meta mt-4">
-        Posted by
-        <a href="<?php echo BASE_URL . '/public/user.php?user_id=' . $post['user_id'];?>">
-            <?php echo $post['firstname'] . " " . $post['lastname']; ?>
-        </a> <span class="border-right">
+        </a>
+        <!--print author-->
+        <p class="post-meta mt-4">
+            Posted by
+            <a href="<?php echo BASE_URL . '/public/user.php?user_id=' . $post['user_id'];?>">
+                <?php echo $post['firstname'] . " " . $post['lastname']; ?>
+            </a> <span class="border-right">
                 <!--print dates-->
                     on
                     <?php $post['created'] = new DateTime($post['created']);
                     echo $post['created']->format('M jS Y'); ?>
                     </span>
-        <!--print likes-->
-        <i class="fa fa-heart-o" aria-hidden="true"></i>
-        <?php
+            <!--print likes-->
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+            <?php
                     $likeCount->execute([':postId' => $post['post_id']]);
                     $res = $likeCount->fetch(PDO::FETCH_ASSOC);
                     echo $res['COUNT(*)']; ?>
-            <!--print tags-->
-            <br>
-            <?php foreach (explode(',',$post['tags']) as $tag): ?>
-            <a href="<?php echo BASE_URL . '/public/tag.php?tag=' . $tag?>">
-                <span href="#" class="badge badge-pill badge-default"><?php echo $tag; ?></span></a>
-            <?php endforeach; ?>
-    </p>
-</div>
-<hr>
+                <!--print tags-->
+                <br>
+                <?php foreach (explode(',',$post['tags']) as $tag): ?>
+                <a href="<?php echo BASE_URL . '/public/tag.php?tag=' . $tag?>">
+                    <span href="#" class="badge badge-pill badge-default"><?php echo $tag; ?></span></a>
+                <?php endforeach; ?>
+        </p>
+    </div>
+    <hr>
--- app/views/user/list.php
+++ app/views/user/list.php
@@ -1,60 +1,62 @@
 <?php require VIEW_ROOT . '/user/templates/header.php'; ?>
 <?php require VIEW_ROOT . '/user/templates/sidenav.php'; ?>
- <div id="page-content-wrapper">
-            <div class="container mt-3">
-                <div class="row">
-                    <div class="col-lg-12 col-xs-12">
-  <?php if(isset($_GET['success'])): ?>
-      <div class="alert alert-success" role="alert">Post was successfully <?php echo $_GET['success'] . '!'; ?></div>
-  <?php endif ?>
-  <?php if (isset($_GET['access'])): ?>
-      <div class="alert alert-danger" role="alert">Acess was <?php echo $_GET['access'] . '.';?></div>
-  <?php endif ?>
-
-
-  <h2>Your posts</h2>
-  <?php if (empty($userPosts)): ?>
-    <p>No posts at the moment.</p>
-  <?php else: ?>
-    <table class="table table-responsive table-hover">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Created</th>
-          <th>Updated</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($userPosts as $post): ?>
-          <tr scope ="row">
-            <td><a href="<?php echo BASE_URL; ?>/page.php?post_id=<?php echo $post['post_id']; ?>"><?php echo escape($post['title']); ?></a></td>
-            <td><?php echo $post['created']; ?></td>
-            <td><?php if (isset($post['updated'])) echo $post['updated']; ?></td>
-            <td><a class="btn btn-info" role="button" href="<?php echo BASE_URL; ?>/user/edit.php?post_id=<?php echo $post['post_id']; ?>">Edit</a></td>
-            <td><button class="btn btn-info delete-btn pointer" data-name="<?php echo $post['title'];?>" data-link="<?php echo BASE_URL . '/user/delete.php?post_id=' . $post['post_id']; ?>">Delete</button></td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-    </div>
-    </div>
-  <?php endif; ?>
-
-    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fa fa-arrows-h" aria-hidden="true"></i> Toggle menu</a>
-                    </div>
+<div id="page-content-wrapper">
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-lg-12 col-xs-12">
+                <?php if(isset($_GET['success'])): ?>
+                <div class="alert alert-success" role="alert">Post was successfully
+                    <?php echo $_GET['success'] . '!'; ?>
                 </div>
+                <?php endif ?>
+                <?php if (isset($_GET['access'])): ?>
+                <div class="alert alert-danger" role="alert">Acess was
+                    <?php echo $_GET['access'] . '.';?>
+                </div>
+                <?php endif ?>
+                <h2>Your posts</h2>
+                <?php if (empty($userPosts)): ?>
+                <p>No posts at the moment.</p>
+                <?php else: ?>
+                <table class="table table-responsive table-hover">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Created</th>
+                            <th>Updated</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($userPosts as $post): ?>
+                        <tr scope="row">
+                            <td>
+                                <a href="<?php echo BASE_URL; ?>/page.php?post_id=<?php echo $post['post_id']; ?>">
+                                    <?php echo escape($post['title']); ?>
+                                </a>
+                            </td>
+                            <td>
+                                <?php echo $post['created']; ?>
+                            </td>
+                            <td>
+                                <?php if (isset($post['updated'])) echo $post['updated']; ?>
+                            </td>
+                            <td><a class="btn btn-info" role="button" href="<?php echo BASE_URL; ?>/user/edit.php?post_id=<?php echo $post['post_id']; ?>">Edit</a></td>
+                            <td>
+                                <button class="btn btn-info delete-btn pointer" data-name="<?php echo $post['title'];?>" data-link="<?php echo BASE_URL . '/user/delete.php?post_id=' . $post['post_id']; ?>">Delete</button>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
-        <!-- /#page-content-wrapper -->
-
-
+        <?php endif; ?>
+        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fa fa-arrows-h" aria-hidden="true"></i> Toggle menu</a>
     </div>
-
-
-
+</div>
+</div>
 <!--script for sweet-alert popups on delete-->
 <script src="<?php echo BASE_URL?>/assets/js/delete-alerts.js"></script>
-
 <?php require VIEW_ROOT . '/user/templates/footer.php'; ?>
